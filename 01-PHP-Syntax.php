aee306c76c197106d3bd60dd22d4ab834c8cc73e
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-10 16:39:49 +0100

    PHP Syntax Voltooid (#1)

---
+++ 01-PHP-Syntax.php
@@ -0,0 +1,7 @@
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";//example
+echo "My house is " . $COLOR . "<br>";//example
+echo "My boat is " . $coLOR . "<br>";//example
+echo "Hello World!"//exercise
+?>
