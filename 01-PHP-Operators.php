2ef3123b971e195ceff063714f8525df9b9a3636
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-11 14:48:01 +0100

    01-PHP Operators Voltooid (#1)

---
+++ 01-PHP-Operators.php
@@ -0,0 +1,64 @@
+<?php
+// PHP Arithmetic Operators
+/*
++ addition
+- substraction
+* multiplication 
+/ Division
+% Modulus
+** Exponentiation 
+
+PHP assigment Operators
+x = y	x = y	The left operand gets set to the value of the expression on the right	
+x += y	x = x + y	Addition	
+x -= y	x = x - y	Subtraction	
+x *= y	x = x * y	Multiplication	
+x /= y	x = x / y	Division	
+x %= y	x = x % y	Modulus
+
+PHP Comparison Operators
+==	Equal
+===	Identical
+!=	Not equal
+<>	Not equal
+!==	Not identical
+>	Greater than
+<	Less than
+>=	Greater than or equal to
+<=	Less than or equal to
+<=>	Spaceship
+
+PHP Increment / Decrement Operator
+++$x	Pre-increment	Increments $x by one, then returns $x	
+$x++	Post-increment	Returns $x, then increments $x by one	
+--$x	Pre-decrement	Decrements $x by one, then returns $x	
+$x--	Post-decrement	Returns $x, then decrements $x by one
+
+PHP Logical Operators
+and	And	$x and $y	True if both $x and $y are true	
+or	Or	$x or $y	True if either $x or $y is true	
+xor	Xor	$x xor $y	True if either $x or $y is true, but not both	
+&&	And	$x && $y	True if both $x and $y are true	
+||	Or	$x || $y	True if either $x or $y is true	
+!	Not	!$x	True if $x is not true
+
+PHP String Operators
+.	Concatenation	$txt1 . $txt2	Concatenation of $txt1 and $txt2	
+.=	Concatenation assignment	$txt1 .= $txt2	Appends $txt2 to $txt1
+
+PHP Array Operators
++	Union	$x + $y	Union of $x and $y	
+==	Equality	$x == $y	Returns true if $x and $y have the same key/value pairs	
+===	Identity	$x === $y	Returns true if $x and $y have the same key/value pairs in the same order and of the same types	
+!=	Inequality	$x != $y	Returns true if $x is not equal to $y	
+<>	Inequality	$x <> $y	Returns true if $x is not equal to $y	
+!==	Non-identity	$x !== $y	Returns true if $x is not identical to $y
+
+PHP Conditional Assigment Operators
+?:	Ternary
+??	Null coalescing
+*/
+
+//exercise
+echo(10 * 5);
+?>
