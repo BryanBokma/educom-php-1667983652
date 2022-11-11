bddbacf7b0b63c7e4af1398869405a036348cde4
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-11 12:31:52 +0100

    01-PHP Strings Voltooid (#1)

---
+++ 01-PHP-Strings.php
@@ -0,0 +1,9 @@
+<?php
+echo strlen("Hello World!");//outputs 12
+echo str_word_count("Hello world!"); // outputs 2
+echo strrev("Hello world"); // outputs !dlrow olleH
+echo strpos("Hello world!", "World"); //outputs 6
+echo str_replace("world", "Dolly", "Hello world!"); //outputs Hello Dolly!
+
+echo str_word_count("Hello World!");
+?>
