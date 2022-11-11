88e079d538d7cff7c314c3fb7a0ba35bdc2ca3a8
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-11 13:42:10 +0100

    01-PHP Math Voltooid (#1)

---
+++ 01-PHP-Math.php
@@ -0,0 +1,21 @@
+<?php
+// (Pi function)
+echo(pi()); // returns 3.1415926535898 
+
+//PHP min()and max() Functions
+echo(min(0, 150, 30, 20, -8, -200));// returns -200
+echo(max(0, 150, 30, 20, -8, -200));// returns 150
+
+// PHP abs() Function
+echo(abs(-6.7)); //returns 6.7
+
+// PHP sqrt() Function
+echo(sqrt(64)); //returns 8
+
+// PHP round() function
+echo(round(0.60)); // returns 1
+echo(round(0.49)); // returns 0
+
+// PHP Random numbers
+echo (rand(10, 100));
+?>
