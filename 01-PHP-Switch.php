853b7a322f44a128b5b1a7e03ccce8376c60243a
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-11 15:30:12 +0100

    01-PHP Switch Voltooid (#1)

---
+++ 01-PHP-Switch.php
@@ -0,0 +1,14 @@
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Hello";
+        break;
+      case "green":
+        echo "Welcome";
+        break;
+      default:
+        echo "Your favorite color is neither red nor green!";
+}//end switch
+?>
