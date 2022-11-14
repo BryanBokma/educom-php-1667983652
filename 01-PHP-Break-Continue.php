026e0c6903c1c5177ccbfbb4e17d085cb3f77e7b
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-14 10:58:37 +0100

    01-PHP Break-Continue aangepast (#1)

--- 01-PHP-Break-Continue.php
+++ 01-PHP-Break-Continue.php
@@ -6,4 +6,12 @@ for ($x = 0; $x < 10; $x++) {
     }//end if
     echo "The number is: $x <br>";
 }// end for
+
+// PHP continue 
+for ($x = 0; $x < 10; $x++) {
+    if ($x == 4) {
+        continue;
+    }//end if 
+    echo "The number is: $x <br>";
+}//end for
 ?>
