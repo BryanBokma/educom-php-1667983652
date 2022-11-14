1abe32188d8cec4eedca7fc5677594a4a71b3073
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-14 11:21:14 +0100

    01-PHP Associative Arrays Voltooid (#1)

---
+++ 01-PHP Associative Arrays.php
@@ -0,0 +1,13 @@
+<?php
+// PHP Associative Arrays 
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+echo "Peter is " . $age['Peter'] . " years old.";
+
+// Loop through an Associative Array
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+?>
