e54cc8abcd3452403882bdad86c048bb3f51ef6b
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-15 09:53:53 +0100

    02-PHP-Form Required Voltooid (#2)

---
+++ 02-PHP-Form-Required.php
@@ -0,0 +1,69 @@
+<html>
+<body>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail: 
+<input type="text" name="email">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+Website: 
+<input type="text" name="website">
+<span class="error">* <?php echo $websiteErr;?></span>
+<br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+<br><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other
+<span class="error">* <?php echo $genderErr;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit">
+
+</form>
+
+<?php
+// Define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $nameErr = "Email is required";
+    } else {
+        $name = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["website"])) {
+        $nameErr = "";
+    } else {
+        $name = test_input($_POST["website"]);
+    }
+
+    if (empty($_POST["comment"])) {
+        $nameErr = "";
+    } else {
+        $name = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $nameErr = "Gender is required";
+    } else {
+        $name = test_input($_POST["gender"]);
+    }
+}
+?>
+</body>
+</html>
+
+
