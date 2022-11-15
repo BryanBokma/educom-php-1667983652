aba515c9ec61c80d95b0ea7d876980ee285bb735
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-15 12:23:29 +0100

    03-PHP Include Voltooid (#3)

---
+++ 03-PHP-Include-Vervolg.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h1>Welcome to my home page!</h1>
+<?php include 'noFileExists.php';//include/require
+echo "I have a $color $car.";
+
+?>
+<?php include 'footer.php';// exercise
+
+</body>
+</html>
