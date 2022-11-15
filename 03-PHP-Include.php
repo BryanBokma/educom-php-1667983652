5d8a9d00055a39c68cffec693cb3728a94f067c6
Bryan Bokma <bryanbokma4@gmail.com>
2022-11-15 11:52:02 +0100

    03-PHP Include tot en met example 2 Voltooid (#3)

---
+++ 03-PHP-Include.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<div class="Menu">
+<?php include 'Menu.php';?>
+</div>
+
+<h1>Welcome to my home page!</h1>
+<p>Some information</p>
+<p>Some more information</p>
+
+
+</body>
+</html>
