9147ab39e9892c025e01734785235547da2cd503
Bryan Bokma <116903663+BryanBokma@users.noreply.github.com>
2022-11-16 15:05:46 +0100

    04-PHP Constructor Voltooid (#4)

---
+++ 04-PHP-The-Construct-Function.php
@@ -0,0 +1,41 @@
+<?php
+// We see in the example below, that using a constructor saves us from calling the set_name() method which reduces the amount of code:
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+
+$apple = new Fruit("Apple");
+echo $apple->get_name();
+?>
+
+<?php
+// Voorbeeld 2
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit("Apple", "red");
+echo $apple->get_name();
+echo "<br>";
+echo $apple->get_color();
+?>
